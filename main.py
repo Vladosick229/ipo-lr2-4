183c47b8f02ece10ce48cd98a40a7738eabfc9c9
Vlad <vladsevruk8@gmail.com>
2024-09-22 17:32:14 +0300

    4 exercise

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import math
 x = float(input("Введите значение x: "))
 y = float(input("Введите значение y: "))
 z = float(input("Введите значение z: ")) 
-
-b = math.sqrt(10 * (3 * math.sqrt(x) + x**(2/5)) * (math.asin(z) - abs(x - y))**2)
-
-print(f"Значение B: {b}")
+if -1 <= z <= 1:
+  b = math.sqrt(10 * (3 * math.sqrt(x) + x**(2/5)) * (math.asin(z) - abs(x - y))**2)
+  print(f"Значение B: {b}")
+else :
+    print("Значение z должно быть в диапазоне от -1 до 1.")
